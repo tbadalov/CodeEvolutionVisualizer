5dbdb9d9ba941a3b7bef854a9455288d28761a41
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-01-21 00:37:58 +0200

    Add scrolling and axis line

--- src/commit_range_view.js
+++ src/commit_range_view.js
@@ -1,12 +1,43 @@
 const React = require('react');
+const Konva = require('konva');
+
+const BAR_WIDTH = 30;
+const BAR_PADDING = 2;
+const BAR_LAYER_LEFT_MARGIN = 40;
+const Y_AXIS_WIDTH = 100;
+const Y_AXIS_LINE_WIDTH = 6;
+const LABEL_HEIGHT = 40;
+const BAR_BOTTOM_MARGIN = LABEL_HEIGHT + 5;
+const PADDING = 250;
 
 const classNameColorMapping = {};
+let lastDx = 0;
+let lastTimestamp = Date.now();
 
-function drawRectangle(context, leftBottomCornerX, leftBottomCornerY, width, height, color) {
-  let prevColor = context.fillStyle;
+
+function drawStack(layer, stack) {
+  layer.add(new Konva.Rect({
+    fill: stack.color,
+    x: stack.x,
+    y: stack.y,
+    width: stack.width,
+    height: stack.height,
+    scaleY: stack.scaleY,
+    scaleX: stack.scaleX,
+  }));
+  /*let prevColor = context.fillStyle;
   context.fillStyle = color;
   context.fillRect(leftBottomCornerX, leftBottomCornerY, width, height);
-  context.fillStyle = prevColor;
+  context.fillStyle = prevColor;*/
+}
+
+function drawLabel(layer, label) {
+  layer.add(new Konva.Text({
+    text: label.text,
+    x: label.x,
+    y: label.y,
+    rotation: label.rotation,
+  }));
 }
 
 function getRandomColor() {
@@ -18,23 +49,129 @@ function getRandomColor() {
   return color;
 }
 
-function drawBar(context, commit, order) {
-  const leftBottomCornerX = 50 + order * (30 /* width */ + 2 /* margin */);
-  let visualizedChangedLinesCount = 0;
-  commit.changedClasses.forEach(changedClass => {
-    if (!classNameColorMapping[changedClass.className]) {
-      classNameColorMapping[changedClass.className] = { color: getRandomColor() };
-    }
-    const leftBottomCornerY = context.canvas.height - 30 /* vertical margin */ - commit.totalChangedLinesCount * 6 /* height */ + visualizedChangedLinesCount * 6;
-    drawRectangle(context, leftBottomCornerX, leftBottomCornerY, 30 /* width */, changedClass.changedLinesCount * 6, classNameColorMapping[changedClass.className].color);
-    visualizedChangedLinesCount += changedClass.changedLinesCount;
-  });
+function drawBar(layer, bar, order) {
+  for (let i = 0; i < bar.stack.length; i++) {
+    const stack = bar.stack[i];
+    drawStack(layer, stack);
+    drawLabel(layer, bar.label);
+  }
 }
 
-function draw(context, commitsData) {
-  commitsData.commits.forEach((commit, index) => {
-    drawBar(context, commit, index);
+function drawAxis(layer, axis) {
+  layer.add(new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: Y_AXIS_WIDTH,
+    height: layer.height(),
+    fill: '#F0F0F0',
+  }));
+
+  layer.add(new Konva.Rect(axis.line));
+  for (let i = 0; i < axis.segments.length; i++) {
+    const segment = axis.segments[i];
+    layer.add(new Konva.Rect(segment));
+    layer.add(new Konva.Text({
+      text: segment.label,
+      x: segment.x - 20,
+      y: segment.y,
+    }));
+  }
+}
+
+function convertToViewData(commitsData, stageHeight) {
+  const data = {
+    axis: {},
+    bars: [],
+  };
+
+  const axisX = (Y_AXIS_WIDTH - Y_AXIS_LINE_WIDTH) / 2;
+  const axisY = stageHeight;
+  const axisWidth = Y_AXIS_LINE_WIDTH;
+  const axisHeight = stageHeight;
+  const segments = [];
+
+  const largestCommitSize = commitsData.commits.reduce((max, commit) => Math.max(max, commit.totalChangedLinesCount), 0);
+  for (let i = 0; i < 6; i++) {
+    segments.push({
+      x: axisX - 5,
+      y: stageHeight - i * (stageHeight / 5) + 4 / 2,
+      width: 10,
+      height: 4,
+      scaleY: -1,
+      label: Math.floor(i * largestCommitSize / 5),
+      full: '#000000',
+    })
+  }
+  const heightPerLine = stageHeight / largestCommitSize;
+
+  for (let i = 0; i < commitsData.commits.length; i++) {
+    const commit = commitsData.commits[i];
+    const barY = stageHeight;
+    const barX = BAR_LAYER_LEFT_MARGIN + i * (BAR_PADDING + BAR_WIDTH);
+    const barHeight = commit.totalChangedLinesCount * heightPerLine;
+    const barWidth = BAR_WIDTH;
+    const label = {
+      text: commit.commitHash.substr(0, Math.min(commit.commitHash.length, 7)),
+      rotation: -45,
+      x: barX-15,
+      y: barY + 35,
+    }
+    const stack = [];
+    let currentStackHeight = 0;
+    for (let j = 0; j < commit.changedClasses.length; j++) {
+      const changedClass = commit.changedClasses[j];
+      if (!classNameColorMapping[changedClass.className]) {
+        classNameColorMapping[changedClass.className] = { color: getRandomColor() };
+      }
+
+      const stackX = barX;
+      const stackY = barY - currentStackHeight;
+      const stackHeight = changedClass.changedLinesCount * heightPerLine;
+      const stackWidth = barWidth;
+      const stackColor = classNameColorMapping[changedClass.className].color;
+      stack.push({
+        x: stackX,
+        y: stackY,
+        height: stackHeight,
+        width: stackWidth,
+        color: stackColor,
+        scaleY: -1,
+      });
+      currentStackHeight += stackHeight;
+    }
+    data.axis = {
+      line: {
+        x: axisX,
+        y: axisY,
+        width: axisWidth,
+        height: axisHeight,
+        scaleY: -1,
+        fill: '#000000',
+      },
+      segments: segments,
+    };
+    data.bars.push({
+      x: barX,
+      y: barY,
+      height: barHeight,
+      width: barWidth,
+      label: label,
+      stack: stack,
+    });
+  }
+  return data;
+}
+
+function draw(stage, chartLayer, axisLayer, visualData) {
+  drawAxis(axisLayer, visualData.axis);
+  axisLayer.draw();
+  //window.layer = layer;
+  window.stage = stage;
+  window.Konva = Konva;
+  visualData.bars.forEach((bar, index) => {
+    drawBar(chartLayer, bar, index);
   });
+  chartLayer.draw();
 }
 
 function loadData(url) {
@@ -47,39 +184,130 @@ function configCanvasSize(canvas) {
   canvas.height = canvas.parentElement.clientHeight;
 }
 
+function getCanvasSize(domElement) {
+  return {
+    width: domElement.parentElement.clientWidth,
+    height: domElement.parentElement.clientHeight,
+  };
+}
+
+function calcStageWidth(data) {
+  return BAR_LAYER_LEFT_MARGIN + data.commits.length * (BAR_WIDTH + BAR_PADDING);
+}
+
 class CommitRangeView extends React.Component {
   constructor(props) {
     super(props);
-    this.canvasRef = React.createRef();
+    this.diagramContainerRef = React.createRef();
+    this.scrollContainer = React.createRef();
+    this.largeContainer = React.createRef();
   }
 
   componentDidMount() {
-    const canvas = this.canvasRef.current;
-    const canvasContext = canvas.getContext('2d');
-    configCanvasSize(canvas);
+    const scaleBy = 1.02;
+    const diagramContainer = this.diagramContainerRef.current;
+    const scrollContainer = this.scrollContainer.current;
+    const largeContainer = this.largeContainer.current;
+    console.log("stage height = " + (scrollContainer.parentElement.clientHeight ));
+    console.log(scrollContainer.offsetHeight);
+    console.log(scrollContainer.clientHeight);
+    const stage = new Konva.Stage({
+      container: diagramContainer,
+      width: scrollContainer.parentElement.clientWidth + PADDING * 2,
+      height: scrollContainer.parentElement.clientHeight,
+    });
+    window.stage = stage;
+    stage.x(-PADDING);
+    const axisLayer = new Konva.Layer({
+      x: PADDING,
+    });
+    const chartLayer = new Konva.Layer({
+      x: Y_AXIS_WIDTH+PADDING,
+    });
+    stage.add(chartLayer);
+    stage.add(axisLayer);
     loadData(this.props.url)
-      .then(draw.bind(null, canvasContext))
+      .then(data => {
+        return { commits: data.commits.concat(data.commits).concat(data.commits) };
+      })
+      .then(data => {
+        console.log(data);
+        const stageWidth = calcStageWidth(data);
+        console.log("stageWidth=" + stageWidth);
+        largeContainer.style.width = (stageWidth+Y_AXIS_WIDTH) + 'px';
+        const scrollbarHeight = scrollContainer.offsetHeight - scrollContainer.clientHeight;
+        console.log(scrollbarHeight);
+        largeContainer.style.height = scrollContainer.clientHeight + 'px';
+        stage.height(scrollContainer.clientHeight);
+        return data;
+      })
+      .then(data => convertToViewData(data, stage.height()-BAR_BOTTOM_MARGIN))
+      .then(data => {
+        data.bars = data.bars.concat(data.bars).concat(data.bars);
+        return data;
+      })
+      .then(draw.bind(null, stage, chartLayer, axisLayer))
       .catch((error) => console.log(error));
+
+    function repositionStage() {
+      const currentTimestamp = Date.now();
+      lastTimestamp = currentTimestamp;
+      var dx = scrollContainer.scrollLeft
+      var dy = 0;
+      stage.container().style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
+      stage.x(-PADDING);
+      chartLayer.x(-dx + PADDING+Y_AXIS_WIDTH);
+      //stage.y(-dy);
+      chartLayer.draw();
+    }
+    /*stage.on('wheel', (e) => {
+      console.log(e);
+      if (Math.abs(e.evt.deltaX) > 0) {
+        return;
+      }
+      e.evt.preventDefault();
+      var oldScale = stage.scaleX();
+
+      var pointer = stage.getPointerPosition();
+
+      var mousePointTo = {
+        x: (pointer.x - stage.x()) / oldScale,
+        y: (pointer.y - stage.y()) / oldScale,
+      };
+
+      var newScale =
+        e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+      stage.scale({ x: newScale, y: newScale });
+
+      var newPos = {
+        x: pointer.x - mousePointTo.x * newScale,
+        y: pointer.y - mousePointTo.y * newScale,
+      };
+      stage.position(newPos);
+      stage.batchDraw();
+    });*/
+    scrollContainer.addEventListener('scroll', repositionStage);
+    repositionStage();
+    
   }
 
   render() {
     return(
-      <div className="diagram">
-        <canvas
-          className="diagram-canvas"
-          ref={this.canvasRef}
-          onClick={() => {
-            this.props.changeDiagram(
-              'classOverviewView',
-              {
-                className: 'BrewViewController',
-                branch: 'master',
-                startVersion: 5,
-                endVersion: 20,
-              }
-            )
-          }}
-        />
+      <div
+        className="scroll-container"
+        ref={this.scrollContainer}
+      >
+        <div
+          className="large-container"
+          ref={this.largeContainer}
+        >
+          <div
+            className="container"
+            ref={this.diagramContainerRef}
+          >
+          </div>
+        </div>
       </div>
     );
   }
