85517fc229f59666fb7a206cb55f20c5a38b3379
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-01-26 16:27:58 +0200

    Load data in full component and pass to the chart

--- src/commit_range_full.js
+++ src/commit_range_full.js
@@ -2,16 +2,42 @@ const React = require('react');
 const CommitRangeView = require('./commit_range_view');
 const ItemList = require('./item_list');
 
+function loadData(url) {
+  return fetch(url)
+          .then((result) => result.json());
+}
+
 class CommitRangeViewFull extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: undefined,
+      data: {
+        commits: [],
+      },
     };
   }
   
   onDiagramReady(data) {
     this.setState({ items: [].concat(data.items) });
+  }
+
+  componentDidMount() {
+    loadData(this.props.url)
+      .then(data => {
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        /*data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);
+        data.commits = data.commits.concat(data.commits).concat(data.commits).concat(data.commits);*/
+        console.log(data);
+        this.setState({ ...this.state, data: data });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -23,7 +49,7 @@ class CommitRangeViewFull extends React.Component {
           <ItemList items={this.state.items} />
         </div>
         <div className="box-2">
-          <CommitRangeView onReady={this.onDiagramReady.bind(this)} {...this.props} />
+          <CommitRangeView onReady={this.onDiagramReady.bind(this)} {...this.props} data={this.state.data} />
         </div>
       </div>
     );
