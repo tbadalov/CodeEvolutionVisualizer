a4d846c64cce0320068fc29898a7956403b213da
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-01-29 20:39:19 +0200

    Add a button component to switch commits

---
+++ src/call_volume_view/switch_commit_button.js
@@ -0,0 +1,29 @@
+const React = require('react');
+const switchCommitButtonStyle = require('./css/switch_commit_button.less');
+
+class SwitchCommitButton extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const direction = this.props.direction;
+    const classNames = [ "switch-commit-button", "switch-commit-button-circle" ];
+    if (direction == 'prev') {
+      classNames.append("prev-commit-button");
+    } else if (direction == 'next' ) {
+      classNames.append("next-commit-button");
+    };
+
+    return(
+      <a
+        className={ classNames.join(' ') }
+        href="#"
+        onClick={ () => this.props.onSwitchCommitButtonClick(direction) } >
+        { direction == 'prev' ? '<' : '>' }
+      </a>
+    );
+  }
+}
+
+module.exports = SwitchCommitButton;
