78f83704fbd77977afe205612e546a8fff163654
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-01-31 01:56:17 +0200

    Switch to call volume view on commit click

--- src/commit_range_view.js
+++ src/commit_range_view.js
@@ -11,11 +11,30 @@ const LABEL_HEIGHT = 40;
 const BAR_BOTTOM_MARGIN = LABEL_HEIGHT + 5;
 const PADDING = 250;
 const EMPTY_SPACE_TOP_PERCENTAGE = 10;
+let isMouseDown = false;
+let isMouseMoving = false;
 
-const classNameColorMapping = {};
-let lastDx = 0;
-let lastTimestamp = Date.now();
+function mouseMoveListener(event, barDataManager, scrollContainer) {
+  event.preventDefault();
+  if (isMouseDown) {
+    // range selection
 
+  } else {
+    // just hovering
+    const foundBar = barDataManager.barByCoordinate(event.offsetX+scrollContainer.scrollLeft-Y_AXIS_WIDTH, event.offsetY);
+    window.t = event.target;
+    //console.log(event.clientX);
+    console.log(event.offsetX+scrollContainer.scrollLeft);
+    //console.log(scrollContainer.scrollLeft);
+    console.log(foundBar);
+    console.log(event);
+    if (foundBar) {
+      event.target.style.cursor = 'cursor-url';
+    } else {
+      event.target.style.cursor = 'auto';
+    }
+  }
+}
 
 function drawStack(layer, stack) {
   layer.add(new Konva.Rect({
@@ -27,29 +46,60 @@ function drawStack(layer, stack) {
     scaleY: stack.scaleY,
     scaleX: stack.scaleX,
   }));
-  /*let prevColor = context.fillStyle;
-  context.fillStyle = color;
-  context.fillRect(leftBottomCornerX, leftBottomCornerY, width, height);
-  context.fillStyle = prevColor;*/
 }
 
-function drawLabel(layer, label) {
-  layer.add(new Konva.Text({
+function drawLabel(layer, label, onLabelClick) {
+  const text = new Konva.Text({
     text: label.text,
     x: label.x,
     y: label.y,
     rotation: label.rotation,
-  }));
+  });
+  layer.add(text);
+  text.on('mouseenter', function () {
+    stage.container().style.cursor = 'pointer';
+  });
+  text.on('mouseleave', function () {
+    stage.container().style.cursor = 'auto';
+  });
+  text.on('click', function (e) {
+    console.log(e);
+    onLabelClick(e.target.attrs.text);
+  });
 }
 
-function drawBar(layer, bar, order) {
+function drawBar(layer, bar, onLabelClick) {
   const barGroup = new Konva.Group();
+  const stackGroup = new Konva.Group();
+  barGroup.add(stackGroup);
   layer.add(barGroup);
   for (let i = 0; i < bar.stack.length; i++) {
     const stack = bar.stack[i];
-    drawStack(barGroup, stack);
+    drawStack(stackGroup, stack);
   }
-  drawLabel(barGroup, bar.label);
+  drawLabel(barGroup, bar.label, onLabelClick);
+  let rect = null;
+  stackGroup.on('mousemove', function (e) {
+    console.log(e);
+    if (rect) {
+      rect.destroy();
+    }
+    /*rect = new Konva.Rect();
+    rect.x(e.evt.layerX-Y_AXIS_WIDTH);
+    rect.y(e.evt.layerY);
+    rect.width(50);
+    rect.height(50);
+    rect.fill('#fff');
+    barGroup.add(rect);
+    barGroup.draw();*/
+  });
+  barGroup.on('mouseleave', function (e) {
+    if (rect) {
+      rect.destroy();
+      rect = null;
+    }
+    barGroup.draw();
+  });
 }
 
 function drawAxis(layer, axis) {
@@ -74,92 +124,7 @@ function drawAxis(layer, axis) {
 }
 
 
-
-function convertToViewData(commitsData, stageHeight) {
-  const data = {
-    axis: {},
-    bars: [],
-  };
-
-  const axisX = (Y_AXIS_WIDTH - Y_AXIS_LINE_WIDTH) / 2;
-  const axisY = stageHeight;
-  const axisWidth = Y_AXIS_LINE_WIDTH;
-  const axisHeight = stageHeight;
-  const segments = [];
-
-  const largestCommitSize = commitsData.commits.reduce((max, commit) => Math.max(max, commit.totalChangedLinesCount), 0);
-  for (let i = 0; i < 6; i++) {
-    segments.push({
-      x: axisX - 5,
-      y: stageHeight - i * (stageHeight / 5) + 4 / 2,
-      width: 10,
-      height: 4,
-      scaleY: -1,
-      label: Math.floor(i * (largestCommitSize + Math.ceil(EMPTY_SPACE_TOP_PERCENTAGE / 100.0 * largestCommitSize)) / 5),
-      fill: '#000000',
-    })
-  }
-  const heightPerLine = stageHeight / (largestCommitSize + Math.ceil(EMPTY_SPACE_TOP_PERCENTAGE / 100.0 * largestCommitSize));
-
-  for (let i = 0; i < commitsData.commits.length; i++) {
-    const commit = commitsData.commits[i];
-    const barY = stageHeight;
-    const barX = BAR_LAYER_LEFT_MARGIN + i * (BAR_PADDING + BAR_WIDTH);
-    const barHeight = commit.totalChangedLinesCount * heightPerLine;
-    const barWidth = BAR_WIDTH;
-    const label = {
-      text: commit.commitHash.substr(0, Math.min(commit.commitHash.length, 7)),
-      rotation: -45,
-      x: barX-15,
-      y: barY + 35,
-    }
-    const stack = [];
-    let currentStackHeight = 0;
-    for (let j = 0; j < commit.changedClasses.length; j++) {
-      const changedClass = commit.changedClasses[j];
-      if (!classNameColorMapping[changedClass.className]) {
-        classNameColorMapping[changedClass.className] = { color: getRandomColor() };
-      }
-
-      const stackX = barX;
-      const stackY = barY - currentStackHeight;
-      const stackHeight = changedClass.changedLinesCount * heightPerLine;
-      const stackWidth = barWidth;
-      const stackColor = classNameColorMapping[changedClass.className].color;
-      stack.push({
-        x: stackX,
-        y: stackY,
-        height: stackHeight,
-        width: stackWidth,
-        color: stackColor,
-        scaleY: -1,
-      });
-      currentStackHeight += stackHeight;
-    }
-    data.axis = {
-      line: {
-        x: axisX,
-        y: axisY,
-        width: axisWidth,
-        height: axisHeight,
-        scaleY: -1,
-        fill: '#000000',
-      },
-      segments: segments,
-    };
-    data.bars.push({
-      x: barX,
-      y: barY,
-      height: barHeight,
-      width: barWidth,
-      label: label,
-      stack: stack,
-    });
-  }
-  return data;
-}
-
-function draw(stage, chartLayer, axisLayer, visualData, skipAxis) {
+function draw(stage, chartLayer, axisLayer, visualData, skipAxis, onLabelClick) {
   if (!skipAxis) {
     drawAxis(axisLayer, visualData.axis);
     axisLayer.draw();
@@ -170,43 +135,12 @@ function draw(stage, chartLayer, axisLayer, visualData, skipAxis) {
   visualData.bars.forEach((bar, index) => {
     //const barGroup = new Konva.Group();
     //chartLayer.add(barGroup);
-    drawBar(chartLayer, bar, index);
+    drawBar(chartLayer, bar, onLabelClick);
   });
   chartLayer.draw();
 }
 
-function loadData(url) {
-  return fetch(url)
-          .then((result) => result.json());
-}
-
-function configCanvasSize(canvas) {
-  canvas.width = canvas.parentElement.clientWidth;
-  canvas.height = canvas.parentElement.clientHeight;
-}
-
-function getCanvasSize(domElement) {
-  return {
-    width: domElement.parentElement.clientWidth,
-    height: domElement.parentElement.clientHeight,
-  };
-}
-
-function calcStageWidth(data) {
-  return BAR_LAYER_LEFT_MARGIN + data.commits.length * (BAR_WIDTH + BAR_PADDING);
-}
-
-function barsByRange(xStart, xEnd, visualData) {
-  const startBarIndex = Math.floor(Math.max(0, (xStart - BAR_LAYER_LEFT_MARGIN)) / (BAR_PADDING + BAR_WIDTH));
-  const endBarIndex = Math.ceil(Math.max(0, (xEnd - BAR_LAYER_LEFT_MARGIN)) / (BAR_PADDING + BAR_WIDTH));
-  return visualData.bars.slice(startBarIndex, endBarIndex);
-}
-
-function barByCoordinate(x, y, visualData) {
-  const barIndex = Math.floor(Math.max(0, (x - BAR_LAYER_LEFT_MARGIN)) / (BAR_PADDING + BAR_WIDTH));
-}
-
-function repositionStage(barDataManager, stageData, containers) {
+function repositionStage(barDataManager, stageData, containers, onLabelClick) {
   var dx = containers.scrollContainer.scrollLeft
   var dy = 0;
   stageData.chartLayer.destroyChildren();
@@ -216,7 +150,7 @@ function repositionStage(barDataManager, stageData, containers) {
   stageData.stage.container().style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
   //console.log(bars);
   stageData.chartLayer.x(PADDING+Y_AXIS_WIDTH-dx);
-  draw(stageData.stage, stageData.chartLayer, stageData.axisLayer, { axis: axis, bars: visualData.bars }, false);
+  draw(stageData.stage, stageData.chartLayer, stageData.axisLayer, { axis: axis, bars: visualData.bars }, false, onLabelClick);
   //stage.y(-dy);
 }
 
@@ -259,10 +193,12 @@ class CommitRangeView extends React.Component {
       chartLayer,
     };
     scrollContainer.addEventListener('scroll', () => repositionStage(this.barDataManager, this.stageData, this.containers));
+    //largeContainer.addEventListener('mousemove', (event) => mouseMoveListener(event, this.barDataManager, this.containers.scrollContainer));
   }
 
   componentDidUpdate() {
     console.log("updating...")
+    console.log(this.props);
     const diagramContainer = this.diagramContainerRef.current;
     const scrollContainer = this.scrollContainer.current;
     const largeContainer = this.largeContainer.current;
@@ -278,7 +214,14 @@ class CommitRangeView extends React.Component {
     largeContainer.style.height = scrollContainer.clientHeight + 'px';
     stage.height(this.barDataManager.calculateStageHeight());
     
-    repositionStage(this.barDataManager, this.stageData, this.containers);
+    repositionStage(
+      this.barDataManager,
+      this.stageData,
+      this.containers,
+      (commit) => {
+        this.props.onDiagramChange('callVolumeView', {label: commit, classToColorMapping: this.props.classToColorMapping});
+      }
+    );
   }
 
   render() {
