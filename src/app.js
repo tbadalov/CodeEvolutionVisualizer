e2c6eb9659e97109cb199f5b74a718481f56a8c2
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-02-23 15:07:21 +0200

    Render menu in the main app component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const uiConfig = require('./ui_config');
 const CommitRangeView = require('./commit_range_full');
 const ClassOverviewView = require('./class_overview_view/class_overview_view');
 const CallVolumeView = require('./call_volume_view/call_volume_view');
+const ItemList = require('./item_list');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const diagramStyle = require('./css/diagram.css');
@@ -22,6 +23,7 @@ class App extends React.Component {
           changeDiagram: this.changeDiagram.bind(this),
         },
       },
+      menuItems: [],
     }
   }
 
@@ -29,13 +31,33 @@ class App extends React.Component {
     this.state.currentDiagram = diagramName;
     const data = {...this.state.data};
     data.diagramData.props = props;
-    this.setState({currentDiagram: diagramName, data: data});
+    this.setState({currentDiagram: diagramName, data: data, menuItems: []});
+  }
+
+  addMenuItem(menuItem) {
+    this.setState({
+      menuItems: [...this.state.menuItems, menuItem],
+    });
   }
 
   render() {
     const Diagram = this.diagrams[this.state.currentDiagram];
     return(
-      <Diagram url={uiConfig[this.state.currentDiagram].apiUrl} changeDiagram={this.state.data.diagramData.changeDiagram} {...this.state.data.diagramData.props} />
+      <div className="minu-container">
+        <div className="box-1">
+          <div class="select">
+            <select name="slct" id="slct">
+              <option value="1" selected="selected">master</option>
+              <option value="2">dev</option>
+              <option value="3">feature1</option>
+            </select>
+          </div>
+          <ItemList items={this.state.items} />
+        </div>
+        <div className="box-2">
+          <Diagram url={uiConfig[this.state.currentDiagram].apiUrl} addMenuItem={this.addMenuItem} changeDiagram={this.state.data.diagramData.changeDiagram} {...this.state.data.diagramData.props} />
+        </div>
+      </div>
     );
   }
 }
