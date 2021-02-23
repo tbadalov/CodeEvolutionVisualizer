735348a4bf3dc55f3a440b3345f0f0c7dbf36f02
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-02-23 22:53:05 +0200

    Query all class names for selected range in class overview

--- src/class_overview_view/class_overview_view.js
+++ src/class_overview_view/class_overview_view.js
@@ -2,24 +2,47 @@ const React = require('react');
 const ClassOverviewDiagram = require('./class_overview_diagram');
 const DiagramDataLoader = require('../diagram_data_loader');
 const DataConverter = require('./data_converter');
+const ItemList = require('../item_list');
 
 class ClassOverviewView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      items: [],
     };
   }
 
   componentDidMount() {
     const { startCommit, endCommit } = this.props;
-    new DiagramDataLoader().load(
-      this.props.url,
+    const diagramDataLoader = new DiagramDataLoader();
+    diagramDataLoader.load(
+      `${this.props.url}/class_names`,
       {
-        className: 'BrewViewController',
         startCommit,
         endCommit,
       }
-    ).then(rawData => {
+    ).then(classNames => {
+      const items = classNames.map((className, index) => ({
+        label: className,
+        color: this.props.classToColorMapping[className],
+        checked: index === 0,
+      }));
+      this.props.addMenuItem(
+        <ItemList items={items} />
+      );
+      this.setState({items: items});
+      return classNames[0];
+    })
+    .then(className => {
+      return diagramDataLoader.load(
+        this.props.url,
+        {
+          className: className,
+          startCommit,
+          endCommit,
+        }
+      );
+    }).then(rawData => {
       console.log(rawData);
       return rawData;
     })
