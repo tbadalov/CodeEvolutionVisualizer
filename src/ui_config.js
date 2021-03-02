afc66955ce9a8086ec60c582c127a2bff85895a6
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-03-02 17:23:17 +0200

    Use api path without host name for diagram apis

--- src/ui_config.js
+++ src/ui_config.js
@@ -1,11 +1,11 @@
 module.exports = {
   commitRangeView: {
-    apiUrl: 'http://localhost:3000/commit_range_data',
+    apiUrl: '/commit_range_data',
   },
   classOverviewView: {
-    apiUrl: 'http://localhost:3000/class_overview',
+    apiUrl: '/class_overview',
   },
   callVolumeView: {
-    apiUrl: 'http://localhost:3000/call_volume',
+    apiUrl: '/call_volume',
   },
 };
