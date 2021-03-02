e2c7de92805ff26f30b7ec5a8c98c66fdc41ee97
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-03-02 16:40:27 +0200

    Customize webpack config based on mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,76 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: {
-    commit_range_view_js: './src/app.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        exclude: '/node_modules/',
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.less$/i,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
+module.exports = (env, options) => {
+  console.log(options);
+  const plugins = [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: path.resolve(__dirname, "./", "index.html"),
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  ];
+  if (options.mode !== 'production') {
+    plugins.push(
+      new HtmlWebpackPlugin({
+        title: 'Output Management',
+        template: path.resolve(__dirname, "./", "index.html"),
+      }),
+    );
+  } else {
+    plugins.push(
+      new webpack.ProvidePlugin({
+        _: "lodash",
+      })
+    );
+    plugins.push(
+      new HtmlWebpackPlugin({
+        filename: '../../views/index.ejs',
+        title: 'Output Management',
+        template: path.resolve(__dirname, "./", "index.html"),
+      }),
+    );
+  }
+  return {
+    entry: {
+      frontend: './src/app.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: '/node_modules/',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.ejs$/,
+          loader: 'ejs-loader',
+          options: {
+            variable: 'data',
+            interpolate : '\\{\\{(.+?)\\}\\}',
+            evaluate : '\\[\\[(.+?)\\]\\]'
+          }
+        },
+        {
+          test: /\.less$/i,
+          use: ["style-loader", "css-loader", "less-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: plugins,
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, options.mode !== 'production' ? 'dist' : 'public/javascripts'),
+      publicPath: '/javascripts',
+    },
+  };
 };
