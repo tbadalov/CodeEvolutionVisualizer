9fa6aea2f2a8576ec779d947363bb348eedd5cc0
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-04-10 10:38:50 +0300

    Focus camera on the center of a diagram at first load

--- src/call_volume_view/call_volume_diagram.js
+++ src/call_volume_view/call_volume_diagram.js
@@ -3,6 +3,7 @@ const SwitchCommitButton = require('./buttons/switch_commit_button');
 const PlayButton = require('./buttons/play_button');
 const GeneralDiagram = require('../general_diagram');
 const { draw, convertToVisualizationData } = require('./call_volume_diagram_sketcher');
+const CallVolumeDiagramPositioner = require('./call_volume_diagram_positioner');
 
 const scale = 5;
 const circleMarginX = 6;
@@ -22,6 +23,7 @@ const INITIAL_TRUNK_HEIGHT = 45;
 const FLOOR_MARGIN_VERTICAL = 2;
 const INIT_STEM_LENGTH = 10;
 const floors = [];
+let shouldAdaptCamera = true;
 
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -52,6 +54,54 @@ function dragBoundary(pos) {
   return {
     x: pos.x,
     y: newY,
+  };
+}
+
+function calculateSizeOfTheDiagram(visualData, classesArray) {
+  if (visualData.length === 1) {
+    const width = visualData[0].pipes[0].find(pipe => pipe.type === 'rect').width;
+    return {
+      totalWidth: width,
+      leftWidth: width/2,
+      rightWidth: width/2,
+      middleWidth: 0,
+      totalHeight: visualData[0].pipes.reduce((heightSum, pipe) => pipe.type === 'rect' ? pipe.height : pipe.radius),
+    };
+  }
+  const largestWidthFromLeft = Math.max(
+      ...visualData
+        .slice(0, Math.floor(visualData.length / 2))
+        .map(branch => branch.pipes
+            .map(pipe => pipe.type === 'rect' ? pipe.width : pipe.radius)
+            .reduce((widthSum, width) => widthSum + width, 0)
+         )
+  );
+  const largestWidthFromRight = Math.max(
+    ...visualData
+      .slice(Math.floor(visualData.length / 2))
+      .map(branch => branch.pipes
+        .map(pipe => pipe.type === 'rect' ? pipe.width : pipe.radius)
+        .reduce((widthSum, width) => widthSum + width, 0)
+      )
+  );
+  const diagramPositioner = new CallVolumeDiagramPositioner(classesArray);
+  const middleWidth = classesArray.reduce((widthSum, classRecord, index) => {
+    const classPipeWidth = diagramPositioner.pipeWidth(index);
+    return widthSum + classPipeWidth;
+  }, 0);
+  const totalHeight = Math.max(
+    ...visualData
+      .map(branch => branch.pipes
+          .map(pipe => pipe.type === 'rect' ? pipe.height : pipe.radius)
+          .reduce((heightSum, height) => heightSum + height, 0)
+      )
+  );
+  return {
+    totalWidth: largestWidthFromLeft + middleWidth + largestWidthFromRight,
+    leftWidth: largestWidthFromLeft,
+    rightWidth: largestWidthFromRight,
+    middleWidth,
+    totalHeight,
   };
 }
 
@@ -122,7 +172,12 @@ class CallVolumeDiagram extends React.Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.props.rawData !== prevProps.rawData || this.state.primitiveDiagramProps !== prevState.primitiveDiagramProps) {
       console.log(this.props.rawData);
-      const rawData = this.props.rawData.filter(classRecord => classRecord.totalCallAmount > 0);
+      const rawData = this.props.rawData
+        .filter(classRecord => classRecord.totalCallAmount > 0)
+        .map(classRecord => {
+          classRecord.methods = classRecord.methods.sort((method1, method2) => method1.totalCallAmount - method2.totalCallAmount);
+          return classRecord;
+        });
       const visualizationData = convertToVisualizationData(rawData, {
         classToColorMapping: this.props.classToColorMapping,
       });
@@ -131,7 +186,28 @@ class CallVolumeDiagram extends React.Component {
       this.onDraw = () => {
         return konvaShapes;
       }
-      this.forceUpdate();
+      this.positionCameraToCenterAtFirstLoad(rawData, visualizationData);
+    }
+  }
+
+  positionCameraToCenterAtFirstLoad(rawData, visualizationData) {
+    const diagramSize = calculateSizeOfTheDiagram(visualizationData, rawData);
+    const newScale = diagramSize.totalWidth > diagramSize.totalHeight
+      ? 0.7 * this.state.primitiveDiagramProps.stageProps.width / diagramSize.totalWidth
+      : 0.7 * this.state.primitiveDiagramProps.stageProps.height / diagramSize.totalHeight;
+    if (shouldAdaptCamera && this.state.primitiveDiagramProps.stageProps.scaleX !== newScale) {
+      this.setState({
+        primitiveDiagramProps: {
+          ...this.state.primitiveDiagramProps,
+          stageProps: {
+            ...this.state.primitiveDiagramProps.stageProps,
+            scaleX: newScale,
+            scaleY: newScale,
+            x: this.scrollContainerRef.current.clientWidth/2 - (diagramSize.rightWidth - diagramSize.leftWidth)/2 * newScale,
+          },
+        },
+      });
+      shouldAdaptCamera = this.props.rawData.length === 0;
     }
   }
 
