c25ccb990165a0905e85cbb462a1ac69c8a5b805
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-04-11 10:38:57 +0300

    Allow disabling navigation buttons

--- src/navigation_menu_item.js
+++ src/navigation_menu_item.js
@@ -1,17 +1,18 @@
 const React = require('react');
 const navigationMenuItemStyle = require('./css/navigation_menu_item.scss');
 
-function NavigationMenuItem() {
+function NavigationMenuItem(props) {
+
   return (
-    <div className='navigation-menu-item menu-item'>
+    <div className={'navigation-menu-item menu-item' + (props.canGoBack ? '' : ' disabled')}>
       <div className='navigation-menu-item-button'>
         <a className='label'>Back</a>
       </div>
-      <div className='navigation-menu-item-button' style={{marginLeft: '10px'}}>
-        <a className='label'>Next</a>
+      <div className={'navigation-menu-item-button' + (props.canGoForward ? '' : ' disabled')} style={{marginLeft: '10px'}}>
+        <a className='label'>Forward</a>
       </div>
     </div>
-  )
+  );
 }
 
 module.exports = NavigationMenuItem;
