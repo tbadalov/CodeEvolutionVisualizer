b3f41d4764a84f26793b7b8c2d01cfa62be6ff97
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-04-17 15:55:19 +0300

    Set initial params and update dimensions when data is available

--- src/commit_range_view/bar_chart.js
+++ src/commit_range_view/bar_chart.js
@@ -2,31 +2,53 @@ const React = require('react');
 const { convertToVisualData } = require('./data_converter');
 const constants = require('./constants');
 const ColorContext = require('../contexts/color_context');
-const { useContext } = require('react');
+const { useContext, useEffect } = require('react');
+const { draw } = require('./diagram_sketcher');
+const { usePrimitiveDiagramProps } = require('../common');
 
-const scrollContainerRef = React.createRef();
-const largeContainerRef = React.createRef();
+/*const scrollContainerRef = React.createRef();
+const largeContainerRef = React.createRef();*/
+
+const defaultStageProps = {
+  stageProps: {
+    width: 0,
+    height: 0,
+    x: -constants.PADDING,
+  },
+};
 
 function BarChart(props) {
   const colorContext = useContext(ColorContext);
+  const  [primitiveDiagramProps, setStageHeight, setStageWidth, setPrimitiveDiagramProps] = usePrimitiveDiagramProps(defaultStageProps);
+  useEffect(() => {
+    if (props.width) {
+      setStageWidth(props.width - constants.Y_AXIS_WIDTH);
+    }
+    if (props.height) {
+      setStageHeight(props.height);
+    }
+  }, [props.width, props.height]);
   const visualData = convertToVisualData(props.commits, {
     maxHeight: scrollContainerRef.current ? scrollContainerRef.current.clientHeight - constants.BAR_BOTTOM_MARGIN : 0,
     classToColorMapping: colorContext.classToColorMapping,
     isClassDisabled: props.isClassDisabled,
   });
-
+  const onDraw = () => draw(visualData, {
+    ...props,
+  });
 
   return(
-    <GeneralDiagram {...this.state}
+    <GeneralDiagram {...props}
       rootStyle={{
         position: 'absolute',
-        left: Y_AXIS_WIDTH + 'px',
-        width: this.rootContainerRef.current ? this.rootContainerRef.current.clientWidth - Y_AXIS_WIDTH : undefined,
+        left: constants.Y_AXIS_WIDTH + 'px',
+        width: props.width,
       }}
+      primitiveDiagramProps={primitiveDiagramProps}
       scrollContainerRef={scrollContainerRef}
       largeContainerRef={largeContainerRef}
       onContainerScroll={props.onContainerScroll}
-      onDraw={this.convertDataToPrimitiveShapes}>
+      onDraw={onDraw}>
     </GeneralDiagram>
   );
 }
