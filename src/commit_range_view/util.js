5505fd0f16462fbc812c4b291c59978f43ea4b81
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-04-18 00:45:26 +0300

    Ignore disabled classes when calculating largest commit size

--- src/commit_range_view/util.js
+++ src/commit_range_view/util.js
@@ -1,3 +1,10 @@
-export function largestCommitSize(commits) {
-  return commits.reduce((max, commit) => Math.max(max, commit.totalChangedLinesCount), 0);
+const constants = require('./constants');
+
+export function calculateLargestCommitSize(commits, params = {}) {
+  const isClassDisabled = params.isClassDisabled || {};
+  return commits
+    .map(commit => commit.changedClasses)
+    .map(changedClassesInCommit => changedClassesInCommit.filter(changedClass => !isClassDisabled[changedClass.className]))
+    .map(filteredChangedClassesInCommit => filteredChangedClassesInCommit.reduce((sum, changedClass) => sum+changedClass.changedLinesCount, 0))
+    .reduce((max, totalChangedLinesCount) => Math.max(max, totalChangedLinesCount), 0);
 }
