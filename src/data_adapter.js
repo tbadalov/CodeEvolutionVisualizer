ea32380b9d8c8140430d1e50d3353376808a350c
Turkhan Badalov <turkhan.badalov@ut.ee>
2021-04-25 13:22:31 +0300

    Add data adapter

---
+++ src/data_adapter.js
@@ -0,0 +1,23 @@
+class DataAdapter {
+  constructor(dataArray) {
+    this.dataArray = dataArray;
+    this.filtersByIds = {};
+  }
+
+  addFilter(filterId, filterPredicate) {
+    this.filtersByIds[filterId] = filterPredicate;
+  }
+
+  removeFilter(filterId) {
+    delete this.filtersByIds[filterId];
+  }
+
+  getFilteredData() {
+    const filters = Object.values(this.filtersByIds);
+    return this.dataArray.filter(element => {
+      return filters.reduce((isPassing, filter) => isPassing && filter(element), true);
+    });
+  }
+}
+
+module.exports = DataAdapter;
